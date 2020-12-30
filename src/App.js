63b38e70fb9c5326cc9ee22622eb6a181d7258a7
Daniel Park <menoelpis@gmail.com>
2020-12-30 03:40:36 +0000

    Initial Commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+      Hell there!
+      My name is Daniel
+      I'm hungry
+      Oh Yeshua Come into us!
+      </header>
+    </div>
+  );
+}
+
+export default App;
